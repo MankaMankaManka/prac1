d0ff750cbbb51fb509036d1662996b33ff87a23f
MankaMankaManka <109943723+MankaMankaManka@users.noreply.github.com>
2023-03-07 00:08:24 +1030

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,6 +4,58 @@
 #include "Referee.h"
 #include "Player.h"
 
+
+class Referee
+{
+	public:
+	Referee();
+	Player*refGame(Player*player1, Player*player2);
+	char m1;
+	char m2;
+};
+
+Referee::Referee(){
+	
+}
+
+Player* Referee::refGame(Player*player1, Player*player2){
+	m1 = player1->makeMove();
+	m2 = player2->makeMove();
+	
+	if (m1=='R'){
+		
+		if (m2=='R'){
+			return nullptr;
+		}else if (m2=='P'){
+			return player2;
+		}else if (m2=='S'){
+			return player1;
+		}else return nullptr;
+		
+	}else if (m1=='P'){
+		
+		if (m2=='R'){
+			return player1;
+		}else if (m2=='P'){
+			return nullptr;
+		}else if (m2=='S'){
+			return player2;
+		}else return nullptr;
+	
+	}else if (m1=='S'){
+		
+		if (m2=='R'){
+			return player2;
+		}else if (m2=='P'){
+			return player1;
+		}else if (m2=='S'){
+			return nullptr;
+		}else return nullptr;
+		
+	}else return nullptr;
+}
+
+
 int main(){
  Referee a;
  Human b= Human("jeff");
