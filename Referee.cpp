519f8d6d2f6cae44e44d7fd2333bc6c4a68a61e8
MankaMankaManka <109943723+MankaMankaManka@users.noreply.github.com>
2023-03-07 00:15:22 +1030

    Add files via upload

--- Referee.cpp
+++ Referee.cpp
@@ -2,15 +2,6 @@
 #include "Referee.h"
 #include "Player.h"
 
-class Referee
-{
-	public:
-	Referee();
-	Player*refGame(Player*player1, Player*player2);
-	char m1;
-	char m2;
-};
-
 Referee::Referee(){
 	
 }
