a6fe1d73a120939803e2f4c3f15204829f512daa
MankaMankaManka <109943723+MankaMankaManka@users.noreply.github.com>
2023-03-07 00:14:23 +1030

    Add files via upload

--- Referee.cpp
+++ Referee.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include "Human.h"
-#include "Computer.h"
 #include "Referee.h"
 #include "Player.h"
 
--- Referee.h
+++ Referee.h
@@ -1,6 +1,4 @@
 #include <iostream>
-#include "Human.h"
-#include "Computer.h"
 #include "Player.h"
 #ifndef REFEREE_H
 #define REFEREE_H
